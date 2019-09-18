71ed49b4e9f56e54ec9ca5a5c5d4a01b0d60ba87
shenkaiming <shenkaiming0526@gmail.com>
2019-09-18 12:38:09 +0800

    完成google登錄 資料庫合併?

--- app/src/main/java/com/example/myapplication/MenuActivity.java
+++ app/src/main/java/com/example/myapplication/MenuActivity.java
@@ -3,13 +3,34 @@ package com.example.myapplication;
 import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
 
 public class MenuActivity extends Activity {
     Button btnBluetooth;
     Button btnMap;
+    Button sign_out;
+    GoogleSignInClient mGoogleSignInClient;
+    TextView textView_name;
+    TextView textView_mail;
+    TextView textView_id;
+    ImageView photo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +38,43 @@ public class MenuActivity extends Activity {
         setContentView(R.layout.activity_menu);
         btnBluetooth=findViewById(R.id.select_bluetooth);
         btnMap=findViewById(R.id.select_map);
+        sign_out = findViewById(R.id.log_out);
+        textView_name=findViewById(R.id.textView_name);
+        textView_mail=findViewById(R.id.textView_mail);
+        textView_id=findViewById(R.id.textView_id);
+        photo=findViewById(R.id.photo);
+
+        //-------------------------------GOOGLE LOGIN 資料測試-------------------------------
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(MenuActivity.this);
+        if (acct != null) {
+            String personName = acct.getDisplayName();
+            String personGivenName = acct.getGivenName();
+            String personFamilyName = acct.getFamilyName();
+            String personEmail = acct.getEmail();
+            String personId = acct.getId();
+            Uri personPhoto = acct.getPhotoUrl();
+
+            textView_name.setText(personName);
+            textView_mail.setText(personEmail);
+            textView_id.setText(personId);
+            Glide.with(this).load(personPhoto).into(photo);
+        }
+        sign_out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signOut();
+            }
+        });
+
+        //-------------------------------GOOGLE LOGIN 資料測試-------------------------------
 
 
         btnBluetooth.setOnClickListener(new View.OnClickListener() {
@@ -36,7 +94,20 @@ public class MenuActivity extends Activity {
             }
         });
 
+
+
     }
 
+    private void signOut() {
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(MenuActivity.this,"Successfully signed out", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(MenuActivity.this, LoginActivity.class));
+                        finish();
+                    }
+                });
+    }
 
 }
